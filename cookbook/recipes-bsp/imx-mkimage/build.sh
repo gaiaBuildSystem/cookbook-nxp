55e5c6bed3678fbde675f51b08fe2abf2f46370b
Matheus Castello <matheus@castello.eng.br>
2025-11-26 21:24:31 -0300

    recipes-bsp: Sync with the mimic of imx95 EVK
    We do not believe in Tezi. It makes the life harder for BSP
    customizations. We need a u-boot that also know how to flash the
    board properly without relying on Tezi.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-bsp/imx-mkimage/build.sh
+++ cookbook/recipes-bsp/imx-mkimage/build.sh
@@ -21,13 +21,26 @@ fi
 if [ "$SOC_TARGET" == "iMX95" ]; then
     cd $BUILD_PATH/tmp/$MACHINE/imx-mkimage
 
-    make \
-        SOC=$SOC_TARGET \
-        OEI=YES \
-        LPDDR_TYPE=lpddr5 \
-        QSPI_HEADER=./scripts/fspi_header_133 \
-        dtbs=${UBOOT_DTB_NAME} \
-        flash_all
+    if [ "$MACHINE" == "imx95-verdin-evk" ]; then
+        make \
+            SOC=$SOC_TARGET \
+            OEI=YES \
+            LPDDR_TYPE=lpddr5 \
+            QSPI_HEADER=./scripts/fspi_header_133 \
+            dtbs=${UBOOT_DTB_NAME} \
+            flash_all
+
+    elif [ "$MACHINE" == "smarc-imx95" ]; then
+        make \
+            SOC=$SOC_TARGET \
+            OEI=YES \
+            LPDDR_TYPE=lpddr5 \
+            LPDDR_FW_VERSION="_v202409" \
+            REV=B0 \
+            dtbs=${UBOOT_DTB_NAME} \
+            flash_a55
+
+    fi
 
     sudo -k \
         cp -f $BUILD_PATH/tmp/$MACHINE/imx-mkimage/iMX95/flash.bin $BUILD_PATH/tmp/$MACHINE/deploy/
